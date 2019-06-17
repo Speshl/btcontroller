8a79fecb463827700882e5b942bb0598aee2aa3a
Chaz Gregory <speshled@gmail.com>
2019-06-16 23:09:47 -0500

    Switched to char array and struct for commands

--- src/arduino/BlueTooth_LED_Controller.ino
+++ src/arduino/BlueTooth_LED_Controller.ino
@@ -1,98 +1,150 @@
 #include <SoftwareSerial.h>
 
+struct command
+{
+  char animation;
+  int red;
+  int green;
+  int blue;
+  int delay;
+};
+
+typedef struct command Command;
+
 bool debug = true;
 bool lightStatus = false;
 
-SoftwareSerial btSerial(2, 3); // RX, TX  
+SoftwareSerial btSerial(2, 3); 
 // Connect HM10      Arduino Uno
 //     Pin 1/TXD          Pin 2
 //     Pin 2/RXD          Pin 3
-const int NUMCOMMANDS = 6;
-String newCommands [NUMCOMMANDS];
-String commands [NUMCOMMANDS];
-int index = 0;
+const int NUMCOMMANDS = 10;
+const int NUMCOMMANDATTRIBUTES = 5;
+const int MAXCOMMANDSIZE = 5;
+int attributeIndex = 0;
+int commandIndex = 0;
+int saveIndex = 0;
 bool newCommand = false;
+char newCommands [NUMCOMMANDS] [NUMCOMMANDATTRIBUTES] [MAXCOMMANDSIZE];
+Command commands [NUMCOMMANDS];
 
 void setup() {  
   Serial.begin(9600);
-  // If the baudrate of the HM-10 module has been updated,
-  // you may need to change 9600 by another value
-  // Once you have found the correct baudrate,
-  // you can update it using AT+BAUDx command 
-  // e.g. AT+BAUD0 for 9600 bauds
   btSerial.begin(9600);
-  //btSerial.println("["+String(lightStatus)+"]");
   debugMessage("Ready.");
 }
 
 void loop() {  
-if(btSerial.available() > 0){
+  if(btSerial.available() > 0){
     char temp = btSerial.read();
-    debugMessage("Read: "+ String(temp));
     if(temp == '?'){
+      debugMessage("Found ?");
       sendStatus();
     }else if(temp == '('){
+      debugMessage("Found (");
       updateLightStatus(true);
     }else if(temp == ')'){
+      debugMessage("Found )");
       updateLightStatus(false);
     }else if(temp == '['){
-      debugMessage("Found Color Command Start Character");
       memset(newCommands, 0, sizeof(newCommands));
-      index = 0;
+      debugMessage("Found Color Command Start Character");
+      commandIndex = 0;
+      attributeIndex = 0;
+      saveIndex = 0;
     }else if(temp == ']'){
       debugMessage("Found Color Command End Character");
       newCommand = true;
     }else if(temp == '|'){
-      debugMessage("Full Command: "+newCommands[index]);
-      debugMessage("Found Delimiter Character");
-      index++;
+      debugMessage("Found Command Delimiter Character");
+      commandIndex++;
+      attributeIndex = 0;
+      saveIndex = 0;
+    }else if(temp == ','){
+      debugMessage("Found Attribute Delimiter Character");
+      attributeIndex++;
+      saveIndex = 0;
     }else{
-      newCommands[index] += String(temp);
+      newCommands[commandIndex][attributeIndex][saveIndex] = temp;
+      saveIndex++;
     }
   }
   
   if(newCommand){
-    applyNewCommands();
-    debugMessage("New Command Found: "+ getCommandsText());
+    printNewCommands();
+    updateCommands();
+    printCommands();
     newCommand = false;
   }
 }
 
-void updateLightStatus(bool status){
-  debugMessage("Updating Light status: "+boolToString(status));
-  lightStatus = status;
-  sendStatus();
+void printNewCommands(){
+  for(int i=0; i<=commandIndex; i++){
+    for(int j=0; j<=attributeIndex; j++){
+      Serial.println(newCommands[i][j]);
+    }
+  }
+}
+
+void printCommands(){
+  for(int i=0; i<commandIndex; i++){
+    Serial.print(commands[i].red);
+    Serial.print(",");
+    Serial.print(commands[i].green);
+    Serial.print(",");
+    Serial.print(commands[i].blue);
+    Serial.print(",");
+    Serial.print(commands[i].animation);
+    Serial.print(",");
+    Serial.print(commands[i].delay);
+    Serial.print("\r\n");
+  }
+}
+
+void updateCommands(){
+  for(int i=0; i<=commandIndex; i++){
+    for(int j=0; j<=attributeIndex; j++){
+      switch(j){
+        case 0:
+          commands[i].red = atoi(newCommands[i][j]);
+          break;
+        case 1:
+          commands[i].green = atoi(newCommands[i][j]);
+          break;
+        case 2:
+          commands[i].blue = atoi(newCommands[i][j]);
+          break;
+        case 3:
+          commands[i].animation = newCommands[i][j][0];
+          break;
+        case 4:
+          commands[i].delay = atoi(newCommands[i][j]);
+          break;
+      }
+    }
+  }
 }
 
 void sendStatus(){
   debugMessage("Sending Status");
-  btSerial.print("["+ boolToString(lightStatus) +"]");
+  btSerial.print(boolToString(lightStatus));
 }
 
-String boolToString(bool value){
+void updateLightStatus(bool status){
+  //debugMessage("Updating Light status: "+boolToString(status));
+  lightStatus = status;
+  sendStatus();
+}
+
+char * boolToString(bool value){
   if(value){
-    return "true";
+    return "[true]";
   }else{
-    return "false";
+    return "[false]";
   }
 }
 
-void applyNewCommands(){
-  debugMessage("Applying new commands");
-  for(int i=0; i<NUMCOMMANDS; i++){
-    commands[i] = newCommands[i];
-  }
-}
-
-String getCommandsText() {
-  String returnText = "";
-  for(int i=0; i<NUMCOMMANDS; i++){
-    returnText += String(i) + ": " + commands[i] + "\r\n";
-  }
-  return returnText;
-}
-
-void debugMessage(String m){
+void debugMessage(char m[]){
   if(debug){
     Serial.println(m); 
   }
